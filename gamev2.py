a3900b6959df9d606fc294a72488db2e970b12a8
Nixen <67658311+Nixenn@users.noreply.github.com>
2024-09-26 09:02:13 +0100

    Patches, cleaning, balancing, items

--- gamev2.py
+++ gamev2.py
@@ -8,10 +8,11 @@ print("cli rpg game v2 by Nixenn")
 print("report bugs and give suggestions @ github.com/Nixenn\n")
 input("Press enter to begin.")
 
-print("\nThis game uses the time.sleep() function for tiny delays between text, but you may turn it off.")
+print("\nThis game uses the time.sleep() function for tiny delays between text, \
+      but you may turn it off.")
 sleepEnabled: str = str(input("Would you like to enable the time.sleep() function? (Y/n)\n> "))
 if sleepEnabled.lower() == "n":
-    def sleep(awawa: float):
+    def sleep(awawa):
         pass
 else:
     from time import sleep
@@ -42,7 +43,8 @@ def character_creation():
     name = input("Your characters' name (leave blank for random)\n> ")
     if len(name) <= 0:
         diceroll_name = randint(1, 6)
-        names: list = ["tesla cyber", "goblin warrior", "nintendo ninja", "dell optiplex", "ford focus", "subject number five"]
+        names: list = ["tesla cybertruck", "goblin warrior", "nintendo ninja",
+                       "dell optiplex", "ford focus", "subject number five"]
         name = names[diceroll_name - 1]
         print(f"Your name is {name}")
 
@@ -54,20 +56,21 @@ def character_creation():
     if armour == 1 or armour == "1":
         armour = 1
         luck -= 2
-        defence += 25
+        defence += 50
         hp += 10
         print("This is some very strong armour, but it's quite heavy.")
     elif armour == 2 or armour == "2":
         armour = 2
         luck += 1
         atk += 5
-        defence += 10
+        defence += 35
         print("This set is lighter than plate. It's less resistant as a result, \
-but you can move aroundCount a bit better.")
+but you can move around a bit better.")
     else:
         armour = 3
         luck += 4
         hp += 40
+        defence += 25
         print("Plot armour.")
 
     weapon = input("""Your character can use one of the following weapons.
@@ -108,28 +111,29 @@ good about simplicity?")
     # this determines whether has chosen a "class". the classes are melee, ranged, and pure.
     if armour == 1 and weapon == 1:
         luck -= 2
-        defence = 0
         hp // 1.15
         atk += 28
         isclass = "Knight"
         # this is the melee class, made using plate armour and a sword.
-        # it deals very high damage, but doesnt survive easily. pure classes deal critical damage to knights.
+        # it deals very high damage, but doesnt survive easily.
+        # pure classes deal critical damage to knights.
     if armour == 2 and weapon == 3:
         luck += 1
-        hp += 20
-        defence += 10
+        hp += 50
+        defence += 20
         atk += 10
         isclass = "Archer"
         # this is the ranged class, made using chainmail armour and a hunting bow.
-        # this class is quite well roundCounted. it has better survivability than the knight,
+        # this class is quite well rounded. it has better survivability than the knight,
         # and has consistent damage.
     elif armour == 3 and weapon == 4:
         luck = 10
         hp += 45
+        defence += 10
         isclass = "Pure"
         # this is the pure class, running on 2 hours sleep, a cup of coffee, and a dream.
         # it has perfect luck, meaning it has the highest crit chance. however,
-        # these crits only work on the night class. the pure class is also very tanky.
+        # these crits only work on the knight class. the pure class is also very tanky.
         # perfectly balanced, i know.
 
     print(f"""{name}
@@ -155,6 +159,11 @@ Attack power: {atk}""")
         armour_str = "Chainmail (+10 def)"
     elif armour == 3:
         armour_str = "None (+40hp)"
+    hp *= 1.25
+    hp // 1
+    defence *= 1.1
+    defence // 1
+
     return name, armour, weapon, hp, defence, atk, luck, crit, isclass, weapon_str, armour_str
 
 
@@ -162,7 +171,7 @@ class characters():
     # new attributes (these fucking SUCK THE CODE IS SO BAD OML 4 DEEP NESTING)
     # you may also notice that luck works differently now.
     # it is a chance to decrease the damage you take by a significant amount,
-    # rather than a chance to become a god and/or dodge everything. yeah idk why i made that a thing either
+    # rather than a chance to become a god and/or dodge everything like in v1
     name: str
     armour: str
     armour_str: str  # used to display armour in the stats section
@@ -181,7 +190,6 @@ class characters():
         self.name, self.armour, self.weapon, self.hp, self.defence, self.atk, self.luck, \
             self.crit, self.playerIsClass, self.weapon_str, self.armour_str = character_creation()
 
-    # yes i know weapon is an argument. no i dont care. the game runs fine and im too lazy to clean it.
     def action_attack(self, name, atk, crit, luck, name2, hp2, def2, armour2,
                       luck2, game_over, defenceBoost2, isDefending2):
         print(f"{name} is attacking {name2}!")
@@ -199,7 +207,7 @@ class characters():
             critchance = 40
         diceroll = randint(0, 8)
         if diceroll > luck:
-            critchance: int = randint(15, 30)
+            critchance: int = randint(15, 22)
         damage_dealt = atk + (randint(1, 100) / 10)
 
         # damage modifiers (applied after initial calculation, defence goes last.)
@@ -208,13 +216,14 @@ class characters():
             sleep(0.2)
             damage_dealt = damage_dealt / 0.6
 
-        if randint(2, 8) < luck2 and (randint(1, 100) + 10) < (luck2 * 10):
+        if randint(1, 7) < luck2:
             print(f"{name2} got lucky! The damage they take has been reduced!")
             sleep(0.2)
             damage_dealt / 1.8
 
-        round(damage_dealt, 1)
-        damage_dealt -= def2 // 3
+        damage_dealt -= def2 / 3
+        damage_dealt // 1
+        damage_dealt += randint(1, 15)
         hp2 -= damage_dealt
         if hp2 > 0:
             print(f"{name} attacked {name2} for {damage_dealt} damage! Onwards!")
@@ -235,50 +244,47 @@ class characters():
 
     def action_defend(self, name, defence):
         if self.isDefending == 0:
-            defence += 20
-            self.defenceBoost = 20
+            defence += 50
+            self.defenceBoost = 50
             print(f"{name} is blocking the next attack, raising \
-their defence by {20 - self.isDefending}! ({defence} defence)")
-            self.isDefending += 1
+their defence by {50 - self.isDefending}! ({defence} defence)")
+            self.isDefending += 2
             sleep(0.4)
         else:
-            print("You already have an active block! Continuing to fortify it will give less defence.")
-            defence += 20 - self.isDefending
-            self.defenceBoost += (20 - self.isDefending)
+            print("You already have an active block! Continuing to fortify \
+                  it will give less defence.")
+            defence += 50 - self.isDefending
+            self.defenceBoost += (50 - self.isDefending)
             print(f"{name} is blocking the next attack, raising \
 their defence by {20 - self.isDefending}! ({defence} defence)")
-            self.isDefending += 1
+            self.isDefending += 2
             sleep(0.4)
         changeTurn = True
         return defence, changeTurn
 
     def action_item(self, isTurnOver):
-        print("lol this isnt coded yet")
-        sleep(0.2)
-        print("im probably gonna implement a random item list with temporary buffs and/or healing")
-        sleep(0.4)
-        choice = str(input("""ANyways pick an item
+        choice = str(input("""Pick the item you would like to use from the list.
     1. Super Mushroom
     2. Glass (literal glass. wtf.)
-    3. A FUCKING ROCK
+    3. A FUCKING ROCK.
     4. A magic potion, what could it do?
     5. Go back and do something else.
     Your choice (enter a corresponding number): """))
         print("")
 
         if choice == "1":
-            self.hp += 100
-            print("WOW!! you healed for 100hp!")
+            self.hp += 50
+            print("WOW!! you healed for 50hp!")
             sleep(0.3)
             isTurnOver = True
         elif choice == "2":
             self.hp -= 30
-            self.atk += 10
+            self.atk += 15
             print("that was a dumb idea.")
             sleep(0.1)
             print("HP DOWN 30!!")
             sleep(0.15)
-            print("ATK UP 10!!")
+            print("ATK UP 15!!")
             sleep(0.15)
             if self.hp <= 0:
                 print("...")
@@ -290,19 +296,19 @@ their defence by {20 - self.isDefending}! ({defence} defence)")
         elif choice == "3":
             rockRoll = randint(1, 3)
             if rockRoll == 1:
-                self.defence += 20
-                print("ok what the fuck is going o-")
+                self.defence += 35
+                print("tastes.. crunchy.")
                 sleep(0.1)
-                print("DEFENCE UP 20!!")
+                print("DEFENCE UP 35!!")
                 sleep(0.2)
             else:
                 self.hp -= 25
-                self.defence * 0.8
-                print("ok what the fu- ow..")
+                self.defence += 30
+                print("I think I chipped a tooth..")
                 sleep(0.1)
                 print("HP DOWN 25!!")
                 sleep(0.2)
-                print("DEFENCE DOWN 20%!!")
+                print("DEFENCE UP 30!!")
                 sleep(0.2)
                 if self.hp <= 0:
                     print("...")
@@ -330,20 +336,20 @@ their defence by {20 - self.isDefending}! ({defence} defence)")
                 sleep(0.1)
             elif potionRoll == 2:
                 self.hp += 120
-                print("It was a health potion! hp UP 120!!")
-                sleep(0.1)
-                print("HP UP 120!!")
-                sleep(0.2)
+                print("It was a health potion! you healed for 120hp!")
+                sleep(0.3)
             elif potionRoll == 3:
                 self.isDefending = 0
                 self.defenceBoost = 0
-                print("BONUS!! Your block state was reset, keeping your bonus defence and rendering it indestructable!")
+                print("BONUS!! Your block state was reset, \
+                      keeping your bonus defence and rendering it indestructable!")
                 sleep(0.3)
             elif potionRoll == 4:
                 self.luck -= 8
                 print("OHN O AN UNHEALTHY POTION")
                 sleep(0.1)
                 print("LUCK DOWN 8!!")
+                sleep(0.2)
             else:
                 self.atk += 5
                 self.hp += 20
@@ -407,21 +413,29 @@ while not game_over:
                     Your choice (enter a corresponding number): """))
         print("")
         if PR.hp <= 0 or PR2.hp <= 0:
-            print("An error occured! Error code: IBHP1")
+            print("An error occured! Error code: both_players_dead")
+            input()
+            exit()
         if player_choice == "1":
-            PR2.hp, game_over, changeTurn, PR2.defenceBoost, PR2.isDefending, PR2.defence = PR.action_attack(PR.name, PR.atk, PR.crit, PR.luck, PR2.name, PR2.hp, PR2.defence, PR2.armour, PR2.luck, game_over, PR2.defenceBoost, PR2.isDefending)
+            PR2.hp, game_over, changeTurn, PR2.defenceBoost, \
+                PR2.isDefending, PR2.defence = \
+                PR.action_attack(PR.name, PR.atk, PR.crit, PR.luck, PR2.name, PR2.hp,
+                                 PR2.defence, PR2.armour, PR2.luck, game_over,
+                                 PR2.defenceBoost, PR2.isDefending)
         elif player_choice == "2":
             PR.defence, changeTurn = PR.action_defend(PR.name, PR.defence)
         elif player_choice == "3":
             changeTurn = PR.action_item(changeTurn)
         elif player_choice == "4":
-            changeTurn = PR.action_viewstats(PR.hp, PR.atk, PR.defence, PR.luck, PR.playerIsClass, changeTurn)
+            changeTurn = PR.action_viewstats(PR.hp, PR.atk, PR.defence,
+                                             PR.luck, PR.playerIsClass, changeTurn)
         else:
             print("That's not a valid option! Try again!")
             changeTurn = False
             continue
     except (TypeError):
-        print("Something went wrong! But the game should continue just fine. (Error code: TPF)")
+        print("Something went wrong! But the game should continue just fine. \
+              (Error code: turn_protection_failed)")
     finally:
         if changeTurn:
             if turn == 1:
@@ -430,9 +444,3 @@ while not game_over:
                 turn -= 1
         else:
             continue
-    if PR.hp <= 0 and PR2.hp <= 0:
-        print("An error occured.")
-        sleep(0.2)
-        print("Error code: both_players_dead\nPlease report this on Github.")
-        sleep(5)
-        exit()
